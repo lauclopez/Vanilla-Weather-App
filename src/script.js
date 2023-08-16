368994c00ceb2b3c1469f092ff9fa513f28a4239
lauclopez <lozanoabello18@gmail.com>
2023-08-16 10:33:36 -0400

    showing the current temperature, wind and humidity

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+  
+
+}
+
+let apiKey = "829cdd54fb23609d8fcbe9fc3a1ceb51";
+let city = "paris"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
