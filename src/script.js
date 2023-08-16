0b73fbae82daf33f32cd90e1f668126466afff45
lauclopez <lozanoabello18@gmail.com>
2023-08-16 10:45:06 -0400

    updated the time in html with js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes <10 ) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ]
+  let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+}
+
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -8,9 +32,10 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-  
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML =formatDate(response.data.dt*1000);
 
 }
 
